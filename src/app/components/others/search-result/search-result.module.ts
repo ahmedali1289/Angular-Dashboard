7d613b426a57ac80903ce39a989f81f1e12aa48c
rukhsar624 <rukhsaranwar56@gmail.com>
2023-07-11 11:12:27 +0500

    remove all comment and resolve errors

--- src/app/components/others/search-result/search-result.module.ts
+++ src/app/components/others/search-result/search-result.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from '../../../shared/shared.module';
-
 import { SearchResultRoutingModule } from './search-result-routing.module';
 import { SearchResultComponent } from './search-result.component';
-import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
+
 
 
 @NgModule({
