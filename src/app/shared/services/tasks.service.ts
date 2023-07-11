10714c253dfe0c3adc399c6ca747b9fa24889267
rukhsar624 <rukhsaranwar56@gmail.com>
2023-07-11 10:45:49 +0500

    add same changes

--- src/app/shared/services/tasks.service.ts
+++ src/app/shared/services/tasks.service.ts
@@ -1,9 +1,4 @@
-import { Injectable } from '@angular/core';
 import * as data from '../../shared/data/tasks/task';
-
-@Injectable({
-  providedIn: 'root'
-})
 export class TasksService {
   
   public Tasks: any[] = data.tasks;
